45d359860591871c050a205d89334f89e43a6cf8
harvy666 <harvyhun@gmail.com>
2023-01-28 01:04:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let checkbox1 = document.getElementById("room1");
+console.log(checkbox1.checked);
+
+let checkbox2 = document.getElementById("room2");
+console.log(checkbox2.checked);
+
+let checkbox3 = document.getElementById("room3");
+console.log(checkbox3.checked);
+
+let checkbox4 = document.getElementById("room4");
+console.log(checkbox4.checked);
+
+//second commit
