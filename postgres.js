bf74e888ebe77672f5923b37f4f751f38c07a83f
harvy666 <harvyhun@gmail.com>
2023-02-06 13:27:27 +0100

    add Promise based (maybe?) connection

--- postgres.js
+++ postgres.js
@@ -1,5 +1,6 @@
 import pkg from "pg";
 const { Client } = pkg;
+
 const client = new Client({
   user: "postgres",
   host: "localhost",
@@ -7,10 +8,17 @@ const client = new Client({
   password: "palacsinta",
   port: 5432,
 });
-client.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+
+//DEFAULT JÓ megoldás
+// client.connect(function (err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
+
+client
+  .connect()
+  .then(() => console.log("Connected!"))
+  .catch((e) => console.log());
 
 client.query(
   "CREATE TABLE rooms (id serial PRIMARY KEY, label  VARCHAR(255),checked BOOLEAN NOT NULL)",
@@ -23,6 +31,12 @@ client.query(
   "INSERT INTO rooms (label,checked) VALUES ('room1',true)",
   (err, res) => {
     console.log(err, res);
-    client.end();
+    // client.end();
   }
 );
+
+// client
+//   .query("SELECT NOW()")
+//   .then((result) => console.log(result))
+//   .catch((e) => console.error(e.stack))
+//   .then(() => client.end());
