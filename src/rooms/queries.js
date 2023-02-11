ff63094ff9892aeed829e8d70a2e2898b0c60306
harvy666 <harvyhun@gmail.com>
2023-02-11 13:52:14 +0100

    basic refactor NOT WORKING

---
+++ src/rooms/queries.js
@@ -0,0 +1,14 @@
+//repo réteg?
+
+const getRooms = "Select * from rooms";
+// const getStudentById = "Select * from students where id=$1";
+// const checkEmailExists = "Select s from students s where s.email=$1";
+// const addStudent =
+//   "Insert into students (name,email,age,dob) VALUES ($1,$2,$3,$4)";
+
+// const removeStudent = "Delete from students where id=$1";
+// const updateStudent = "Update students SET name=$1 where id=$2";
+
+module.exports = {
+  getRooms,
+};
